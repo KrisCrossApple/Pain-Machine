ff5c6554c0a48e8544152795747b87da6a2bae2b
KrisCrossApple <167768508+KrisCrossApple@users.noreply.github.com>
2024-05-18 23:43:54 +0930

    Update MainPain.java

--- src/main/MainPain.java
+++ src/main/MainPain.java
@@ -17,6 +17,7 @@ public class MainPain extends Mod{
         //Detects when something dies!
         Events.on(UnitDestroyEvent.class, dead ->{
             // This code will kill me from the .isPlayer()
+            Vars.ui.hudfrag.showToast("if this is because the dead.unit.isPlayer() is acting up i swear to god");
             if(dead.unit.isPlayer()){
                 Vars.ui.hudfrag.showToast("If you see this Kris fucked up. :3");
                 Core.app.exit();
