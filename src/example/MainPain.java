bb36840087f6747f98171199178f5f0257e815a2
KrisCrossApple <167768508+KrisCrossApple@users.noreply.github.com>
2024-05-18 22:52:59 +0930

    Update and rename ExampleJavaMod.java to MainPain.java

---
+++ src/example/MainPain.java
@@ -0,0 +1,30 @@
+package main;
+
+import arc.*;
+import arc.util.*;
+import mindustry.*;
+import mindustry.content.*;
+import mindustry.game.EventType.*;
+import mindustry.gen.*;
+import mindustry.mod.*;
+import mindustry.ui.dialogs.*;
+
+public class mainpain extends Mod{
+
+    public mainpain(){
+        Log.info(":3 This is kris! I am inside your walls! :3");
+
+        //listen for game load event
+        Events.on(UnitDestroyEvent.class, Unit -> {
+            if(Unit.isPlayer()){
+                Vars.ui.hudfrag.showToast("If you see this Kris fucked up. :3");
+        };
+    );
+    }
+
+    @Override
+    public void loadContent(){
+        Log.info("Doot!");
+    }
+
+}
