2f8958f9d1183ba79b27f54c94d5e863649f71c1
KrisCrossApple <167768508+KrisCrossApple@users.noreply.github.com>
2024-05-19 12:06:36 +0930

    DELETE

--- src/main/MainPain.java
+++ src/main/MainPain.java
@@ -6,13 +6,16 @@ import arc.Events;
 import mindustry.*;
 import mindustry.content.*;
 import mindustry.game.EventType.*;
+import mindustry.game.Saves;
+import mindustry.io.*;
 import mindustry.gen.*;
 import mindustry.mod.*;
 import mindustry.ui.dialogs.*;
 import mindustry.core.*;
+import mindustry.game.Saves.*;
 
 public class MainPain extends Mod{
-
+    public Saves saves;
     public MainPain(){
         Log.info("Warning! This mod makes it so that if you die the game crashes and then it WILL DELETE YOUR SAVES. remember BACK UP YOUR SAVES.");
 
@@ -30,6 +33,7 @@ public class MainPain extends Mod{
             // PAIN IN MY *BLEEP* :3
             if(event.unit.isPlayer()){
                 Core.app.exit();
+                saves.deleteAll();
                 Vars.ui.hudfrag.showToast("If this shows up again :3");
             }
         });
