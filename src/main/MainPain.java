7755f8f47e84509795a9147284cdf6e935734320
KrisCrossApple <167768508+KrisCrossApple@users.noreply.github.com>
2024-05-20 20:35:47 +0930

    Why

--- src/main/MainPain.java
+++ src/main/MainPain.java
@@ -5,6 +5,7 @@ import arc.util.*;
 import arc.Events;
 import mindustry.*;
 import mindustry.content.*;
+import mindustry.game.EventType;
 import mindustry.game.EventType.*;
 import mindustry.game.Saves;
 import mindustry.io.*;
@@ -15,20 +16,9 @@ import mindustry.core.*;
 import mindustry.game.Saves.*;
 
 public class MainPain extends Mod{
-    public Saves saves;
     public MainPain(){
-        Log.info("Warning! This mod makes it so that if you die the game exits.");
-
-        Events.on(ClientLoadEvent.class, shenanagins ->{
-            BaseDialog dialog = new BaseDialog("Warning!");
-            dialog.cont.add("By Kris's Pain Machine!").pad(10).row();
-            dialog.cont.add("This mod has PERMADEATH and wont let you load your saves. Be warned").pad(10).row();
-            dialog.cont.button("Ok.", dialog::hide).pad(10).row();
-            dialog.show();
-        });
-
-        Events.on(SaveLoadEvent.class, nill ->{
-            Core.app.exit();
+        Events.run(EventType.Trigger.update, () -> {
+        Log.info(Vars.player.unit().health);
         });
 
         // Detects when something dies :3
